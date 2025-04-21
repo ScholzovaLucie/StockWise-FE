be36572883c060d208966d40c5ce30dc3466294a
Scholzova <91408343+ScholzovaLucie@users.noreply.github.com>
2025-04-21 10:03:45 +0200

    production url

--- services/apiClient.js
+++ services/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://stockwise-be-production-7684.up.railway.app" /*"http://localhost:8000/api",*/,
+    "https://stockwise-be-production-7684.up.railway.app/api/" /*"http://localhost:8000/api",*/,
   withCredentials: true, // Používáme cookies pro tokeny
 });
 
